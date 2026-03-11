38117d50c5ee15cb323c65e01dbdc46bd643959c
Minessp <minestadionchannel@gmail.com>
2026-03-11 14:58:23 -0300

    Baseline

---
+++ src/main/java/br/com/msp/findurbuddy/FindurbuddyApplication.java
@@ -0,0 +1,13 @@
+package br.com.msp.findurbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindurbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindurbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/msp/findurbuddy/FindurbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msp.findurbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindurbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
